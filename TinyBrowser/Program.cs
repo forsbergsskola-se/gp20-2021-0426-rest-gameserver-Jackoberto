69afee078e0c6fe8a7158a87e0710bad8e33db50
Jackoberto <jackoberto01@gmail.com>
2021-04-27 15:54:21 +0200

    Adds TinyBrowser project

---
+++ TinyBrowser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace TinyBrowser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        string HttpRequest()
+        {
+            var result = string.Empty;
+            using (var tcpClient = new TcpClient("www."))
+        }
+    }
+}
