b6a29987e44d5c9f59f4b84d959d5c121fb47362
Jackoberto <jackoberto01@gmail.com>
2021-05-06 10:24:37 +0200

    Added IStation Interface and downloaded scooters.json

---
+++ LameScooter/Data/Station.cs
@@ -0,0 +1,8 @@
+﻿namespace LameScooter.Data
+{
+    public class Station : IStation
+    {
+        public string Name { get; set; }
+        public int BikesAvailable { get; set; }
+    }
+}
--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -6,7 +6,7 @@ namespace LameScooter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
