3816c622068593c3caea461cbea1039a90a16fd7
Jackoberto <jackoberto01@gmail.com>
2021-05-06 18:01:35 +0200

    Adds in implementation for Database usages

--- LameScooter/RentalServices/DatabaseLameScooterRental.cs
+++ LameScooter/RentalServices/DatabaseLameScooterRental.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace LameScooter.RentalServices
@@ -8,17 +10,28 @@ namespace LameScooter.RentalServices
     {
         public async Task<int> GetScooterCountInStation(string stationName)
         {
+            if (stationName.Any(char.IsNumber))
+                throw new ArgumentException($"{stationName} Contains Numbers This Is Not Allowed");
             var client = new MongoClient("mongodb://localhost:27017");
-            var databaseNames = await client.ListDatabaseNamesAsync();
-            while (await databaseNames.MoveNextAsync())
+            var database = client.GetDatabase("scooters");
+            var collection = database.GetCollection<BsonDocument>("stations");
+            var fieldDef = new StringFieldDefinition<BsonDocument, string>("name");
+            var filter = Builders<BsonDocument>.Filter.Eq(fieldDef, stationName);
+            var objects= collection.Find(filter);
+            var cursor = await objects.ToCursorAsync();
+
+            while (await cursor.MoveNextAsync())
             {
-                foreach (var s in databaseNames.Current)
+                foreach (var s in cursor.Current)
                 {
-                   Console.WriteLine(s); 
+                    foreach (var bsonElement in s.Elements)
+                    {
+                        if (bsonElement.Name == "bikesAvailable")
+                            return (int)bsonElement.Value.AsDouble;
+                    }
                 }
             }
-
-            throw new NotImplementedException();
+            throw new NotFoundException(stationName);
         }
     }
 }
