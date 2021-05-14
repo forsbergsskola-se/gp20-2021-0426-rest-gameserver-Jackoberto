9636fdb40b41a30603090157bc6f9abedca071e1
Jackoberto <jackoberto01@gmail.com>
2021-05-14 12:07:06 +0200

    Added start of httpclient

--- MMO-RPG-Client/Assets/Scripts/HttpHandler.cs
+++ MMO-RPG-Client/Assets/Scripts/HttpHandler.cs
@@ -1,7 +1,14 @@
-﻿public class HttpHandler : IHttpHandler
+﻿using System;
+using System.Net.Http;
+
+public class HttpHandler : IHttpHandler
 {
+    private HttpClient Client { get; }
     public HttpHandler()
     {
-        
+        Client = new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:5001/api/")
+        };
     }
 }
