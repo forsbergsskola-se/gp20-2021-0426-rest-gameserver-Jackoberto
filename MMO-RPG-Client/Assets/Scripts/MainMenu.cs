ec51f3160be86e9d9a8290862b26e9e4eec99452
Jackoberto <jackoberto01@gmail.com>
2021-05-14 17:03:08 +0200

    Added Two Implementations For Interacting With The Server

---
+++ MMO-RPG-Client/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour, INeedHttpHandler
+{
+    Task<Player[]> players;
+
+    void OnGUI(){
+        if (players?.IsCompleted == false)
+        {
+            GUILayout.Label("Waiting For Response");
+            return;
+        }
+
+        if (GUILayout.Button("Search Players"))
+        {
+            players = HttpHandler.GetAllPlayers();
+            return;
+        }
+
+        if (GUILayout.Button("Create Player"))
+        {
+            // TODO Add Some Method Call
+            return;
+        }
+
+        foreach (var player in players.Result)
+        {
+            if (GUILayout.Button(player.Name))
+            {
+                // TODO Add Some Method Call
+            }
+        }
+    }
+
+    public IHttpHandler HttpHandler { get; set; }
+}
